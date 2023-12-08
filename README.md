dc0b578181bc8c3f1f9b7018f377a7d5f083d905
Ransom Christofferson <ransom.christofferson@lasp.colorado.edu>
2023-12-08 00:31:15 -0700

    day 4

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Set up by installing [nodejs](https://nodejs.org/en/) and running `npm i` in the
 Run with `npm run solve [day number]`, i.e. `npm run solve 1` for day 1's puzzle.
 
 If the `solve` script doesn't work, use `npm run solveAlt -- src/day1/solution.ts`, or substitute the number for any number from 1-25.
+
+## Helpful links
+
+[Lodash](https://lodash.com/docs/)
---
+++ src/day4/solution.ts
@@ -0,0 +1,43 @@
+import { intersection, sum } from 'lodash';
+import { outputAnswers } from '../output-answers';
+import { officialInput, testInput } from './inputs';
+
+/** maps each line of input to an object representing that scratchcard */
+function parseCards( input: string ) {
+    return input.split( '\n' ).map( line => {
+        return {
+            copies: 1,
+            // count how many winning numbers there are on the scratchcard
+            wins: intersection(
+                ...line.split( ':' )[1].split( '|' ).map( half => {
+                    // parse all multi-digit numbers from the string and convert them to integers
+                    return [ ...half.matchAll(/\d+/g) ].map( match => parseInt(match[0]) )
+                })
+            ).length
+        };
+    });
+}
+
+outputAnswers(
+    testInput,
+    officialInput,
+    testInput,
+    officialInput,
+
+    // function that solves part 1
+    ( input: string ) => sum(
+        // for 0 winning numbers, we get 2^-1 which is 0.5. Flooring this results in a correct score of 0. For all other counts of
+        // winning numbers, Math.floor changes nothing.
+        parseCards( input ).map( card => Math.floor( 2 ** (card.wins - 1) ) )
+    ),
+
+    // function that solves part 2
+    ( input: string ) => {
+        // run through the array of cards, adding copies of cards further down the line according to how many winning numbers a card has
+        // and the number of copies we have of the card, and totaling the number of scratchcards
+        return parseCards( input ).reduce( (total, card, i, cards) => {
+            cards.slice( i + 1, i + 1 + card.wins ).forEach( laterCard => laterCard.copies += card.copies );
+            return total + card.copies;
+        }, 0 );
+    }
+);
